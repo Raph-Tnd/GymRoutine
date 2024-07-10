5130f2f10648356e45a89c7f6c305ea8f3681669
Raphael Tenaud <tenaudraphael@gmail.com>
2024-07-10 20:33:43 +0200

    Removed Tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,22 @@
-import { View, Text } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react'
+import Session from '@/components/Session';
+import Profile from '@/components/Profile';
+
+
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function index() {
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+	return (
+		<Tab.Navigator
+			screenOptions={{
+				headerShown: false
+			}}>
+			<Tab.Screen name="Home" component={Session} />
+			<Tab.Screen name="Profile" component={Profile} />
+		</Tab.Navigator>
+	)
 }
