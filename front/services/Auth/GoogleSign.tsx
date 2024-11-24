d57d26427679dfff0ba665bbdf379e6f82a154ed
Raph-Tnd <tenaudraphael@gmail.com>
2024-11-24 12:00:03 +0100

    front - Login page

--- front/services/Auth/GoogleSign.tsx
+++ front/services/Auth/GoogleSign.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { JwtPayload, jwtDecode } from "jwt-decode";
-import { Button, Pressable, StyleProp, ViewStyle } from "react-native";
+import { Pressable, StyleProp, Text, ViewStyle } from "react-native";
 import { removeValue } from "@/components/global/Storage";
 import React from "react";
 import { useDispatch } from "react-redux";
@@ -9,6 +9,11 @@ import { AppDispatch } from "@/app/store";
 import * as AuthSession from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
 import { mainApi } from "@/features/api/apiSlice";
+import { makeRedirectUri } from "expo-auth-session";
+import { ArrowRight } from "lucide-react-native";
+import { Colors } from "@/style/Colors";
+import LoginStyle from "@/style/LoginStyle";
+import Svg, { Path } from "react-native-svg";
 
 WebBrowser.maybeCompleteAuthSession();
 function TokenToUser(userInfo: JwtPayload): User {
@@ -39,6 +44,9 @@ export function GoogleSign() {
 	const [request, response, promptAsync] = AuthSession.useIdTokenAuthRequest({
 		webClientId:
 			"620859170647-2ih3o74dhd7qmhf4vglrh3ag1jse9bk7.apps.googleusercontent.com",
+		iosClientId:
+			"620859170647-a6dn90fvk2p64klujjie3hefm603cvvc.apps.googleusercontent.com",
+		redirectUri: makeRedirectUri(),
 	});
 	useEffect(() => {
 		const validateUser = async () => {
@@ -59,7 +67,30 @@ export function GoogleSign() {
 		};
 		validateUser();
 	}, [response]);
-	return <Button title={"Login with Google"} onPress={() => promptAsync()} />;
+	return (
+		<Pressable onPress={() => promptAsync()} style={LoginStyle.loginButton}>
+			<Svg width={24} height={24} viewBox="0 0 24 24">
+				<Path
+					d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
+					fill="#4285F4"
+				/>
+				<Path
+					d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z"
+					fill="#34A853"
+				/>
+				<Path
+					d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z"
+					fill="#FBBC05"
+				/>
+				<Path
+					d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z"
+					fill="#EA4335"
+				/>
+			</Svg>
+			<Text style={LoginStyle.loginLabel}>Login with Google</Text>
+			<ArrowRight color={Colors.text_primary} />
+		</Pressable>
+	);
 }
 
 export function GoogleSignOut({
