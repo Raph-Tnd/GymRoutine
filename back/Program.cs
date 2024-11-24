26b96ba5c0a5c1bdc5e6c9971e925044b2235385
Raph-Tnd <tenaudraphael@gmail.com>
2024-11-24 11:17:07 +0100

    back - use cors option for dev

--- back/Program.cs
+++ back/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
